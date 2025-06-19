9fa33c6fdb89414f942f7816fd5f3b99559d477d
kijiwon <kiji1313@gmail.com>
2025-06-19 18:51:02 +0900

    pageGeometry

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -3,9 +3,16 @@ import { Page } from "./Page";
 export const Book = ({ ...props }) => {
   return (
     <group {...props}>
-      {[...pages].map((pageData, index) => (
-        <Page key={index} number={index} {...props} />
-      ))}
+      {[...pages].map((pageData, index) =>
+        index === 0 ? ( // 첫 번째 페이지만 렌더링
+          <Page
+            position-x={index * 0.15}
+            key={index}
+            number={index}
+            {...pageData}
+          />
+        ) : null
+      )}
     </group>
   );
 };
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,12 +1,26 @@
 import { useRef } from "react";
+import { BoxGeometry } from "three";
 
+const PAGE_WIDTH = 1.28;
+const PAGE_HEIGHT = 1.71; // 4:3 종횡비
+const PAGE_DEPTH = 0.003;
+const PAGE_SEGMENTS = 30;
+const SEGMENT_WIDTH = PAGE_WIDTH / PAGE_SEGMENTS; // 각 페이지 geometry가 동일함(런타임때 계산 가능)
+
+const pageGeometry = new BoxGeometry(
+  PAGE_WIDTH,
+  PAGE_HEIGHT,
+  PAGE_DEPTH,
+  PAGE_SEGMENTS,
+  2
+);
 export const Page = ({ number, front, back, ...props }) => {
   const group = useRef();
 
   return (
-    <group {...props}>
+    <group {...props} ref={group}>
       <mesh scale={0.1}>
-        <boxGeometry />
+        <primitive object={pageGeometry} attach={"geometry"} />
         <meshStandardMaterial color="red" />
       </mesh>
     </group>
