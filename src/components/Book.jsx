fab93d7d680372b6f53e2ee11b6c6612ef930835
kijiwon <kiji1313@gmail.com>
2025-06-23 15:00:13 +0900

    page 이동 시 delay

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,17 +1,47 @@
 import { pageAtom, pages } from "./UI";
 import { Page } from "./Page";
 import { useAtom } from "jotai";
+import { useEffect, useState } from "react";
 export const Book = ({ ...props }) => {
   const [page] = useAtom(pageAtom);
+  const [delayedPage, setDelayedPage] = useState(page);
+
+  useEffect(() => {
+    let timeout;
+    const goToPage = () => {
+      setDelayedPage((delayedPage) => {
+        if (page === delayedPage) {
+          return delayedPage;
+        } else {
+          timeout = setTimeout(
+            () => {
+              goToPage();
+            },
+            Math.abs(page - delayedPage) > 2 ? 50 : 150
+          );
+          if (page > delayedPage) {
+            return delayedPage + 1;
+          }
+          if (page < delayedPage) {
+            return delayedPage - 1;
+          }
+        }
+      });
+    };
+    goToPage();
+    return () => {
+      clearTimeout();
+    };
+  }, [page]);
   return (
     <group {...props} rotation-y={-Math.PI / 2}>
       {[...pages].map((pageData, index) => (
         <Page
           key={index}
-          page={page}
+          page={delayedPage}
           number={index}
-          opened={page > index}
-          bookClosed={page === 0 || page === pages.length}
+          opened={delayedPage > index}
+          bookClosed={delayedPage === 0 || delayedPage === pages.length}
           {...pageData}
         />
       ))}
